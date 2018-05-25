dad6c022229a1073ee2bb8fe6c14fbc0918fcb5c
Jean Pierre Richa <jeanpierrericha@Jeans-MacBook-Pro.local>
2018-05-25 20:24:34 +0200

    feat: add walking animation

--- Project2/walkingDog.js
+++ Project2/walkingDog.js
@@ -49,19 +49,19 @@ var tailId     = 11;
 // RLH = Rear Leg Height
 // RLW = Rear Leg Width
 
-var headH    = 1.5;
+var headH    = 1.0;
 var headW    = 1.0;
-var bodyH    = 5.0;
-var bodyW    = 1.0;
-var upperFLH = 2.5;
-var lowerFLH = 1.5;
-var upperFLW = 0.5;
-var lowerFLW = 0.5;
-var upperRLH = 2.5;
-var lowerRLH = 1.5;
-var upperRLW = 0.5;
-var lowerRLW = 0.5;
-var tailH    = 2.5;
+var bodyH    = 1.0;
+var bodyW    = 3.0;
+var upperFLH = 1.5;
+var lowerFLH = 1.0;
+var upperFLW = 0.3;
+var lowerFLW = 0.3;
+var upperRLH = 1.5;
+var lowerRLH = 1.0;
+var upperRLW = 0.3;
+var lowerRLW = 0.3;
+var tailH    = 1.5;
 var tailW    = 0.3;
 
 // The number of nodes to be created, 1 node for each object constituting
@@ -70,8 +70,8 @@ var numNodes = 11;
 
 
 // Angles of the body parts
-var theta = [240, 240, -80, -25, -80, -25, -80, -25, -80, -25, 180, 140];
-
+// var theta = [0, 0, -80, -25, -80, -25, -80, -25, -80, -25, 180, 140];
+var theta = [0, 90, 185, -25, 185, -25, 185, -25, 185, -25, 45, 45];
 var numVertices = 24;
 // Stacking the modelViewMatrices
 var stack = [];
@@ -82,6 +82,9 @@ for (var i = 0; i<=numNodes; i++) figure[i] = createNode(null, null, null, null)
 var vBuffer;
 var modelviewLoc;
 var pointsArray = [];
+
+var Sequence;
+var speed = 10;
 
 
 function scale4(a, b, c){
@@ -108,14 +111,15 @@ function initNodes(Id){
     switch(Id){
 
     case bodyId:
-    t = rotate(theta[bodyId], 1, 1, 1);
+    t = (translate(-7.0, 0.0, 0.0))
+    t = mult(t, rotate(theta[bodyId], 0, 1, 0));
     figure[bodyId] = createNode(t, body, null, headId);
     break;
 
     case headId:
     case head1Id:
     case head2Id:
-    t = translate(0.0, bodyH + 0.5 * headH, 0.5);
+    t = translate(bodyW*0.5+headW*0.5, 0.5*bodyH, 0.0);
     t = mult(t, rotate(theta[head1Id], 1, 0, 0));
     t = mult(t, rotate(theta[head2Id], 0, 1, 0));
     t = mult(t, translate(0.0, -0.5 * headH, 0.0));
@@ -123,57 +127,57 @@ function initNodes(Id){
     break;
 
     case upperFLLId:
-    t = translate(-(bodyW + upperFLW), 0.9 * bodyH, 0.0);
-    t = mult(t, rotate(theta[upperFLLId], 1, 0, 0));
+    t = translate(0.5 * bodyW -(upperFLW), 0.2, -(bodyW/2.0)+0.5*upperFLW);
+    t = mult(t, rotate(theta[upperFLLId], 0, 0, 1));
     figure[upperFLLId] = createNode(t, upperFLL, upperFRLId, lowerFLLId);
     break;
 
     case upperFRLId:
-    t = translate(bodyW + upperFLW, 0.9 * bodyH, 0.0);
-    t = mult(t, rotate(theta[upperFRLId], 1, 0, 0));
+    t = translate(0.5 * bodyW -(upperFLW), 0.2, (bodyW/2.0)-0.5*upperFLW);
+    t = mult(t, rotate(theta[upperFRLId], 0, 0, 1));
     figure[upperFRLId] = createNode(t, upperFRL, upperRLLId, lowerFRLId);
     break;
 
     case upperRLLId:
-    t = translate(-(bodyW + upperRLW), 0.1 * upperRLH, 0.0);
-    t = mult(t, rotate(theta[upperRLLId], 1, 0, 0));
+    t = translate(-(0.5 * bodyW) + upperRLW, 0.2, -(bodyW/2.0)+0.5*upperFLW);
+    t = mult(t, rotate(theta[upperRLLId], 0, 0, 1));
     figure[upperRLLId] = createNode(t, upperRLL, upperRRLId, lowerRLLId);
     break;
 
     case upperRRLId:
-    t = translate(bodyW + upperRLW, 0.1 * upperRLH, 0.0);
-    t = mult(t, rotate(theta[upperRRLId], 1, 0, 0));
+    t = translate(-(0.5 * bodyW) + upperRLW, 0.2, (bodyW/2.0)-0.5*upperFLW);
+    t = mult(t, rotate(theta[upperRRLId], 0, 0, 1));
     figure[upperRRLId] = createNode(t, upperRRL, tailId, lowerRRLId);
     break;
 
     case lowerFLLId:
 
-    t = translate(0.0, upperFLH, 0.0);
-    t = mult(t, rotate(theta[lowerFLLId], 1, 0, 0));
+    t = translate(0.0, upperFLH-0.1, 0.0);
+    t = mult(t, rotate(theta[lowerFLLId], 0, 0, 1));
     figure[lowerFLLId] = createNode(t, lowerFLL, null, null);
     break;
 
     case lowerFRLId:
-    t = translate(0.0, upperFLH, 0.0);
-    t = mult(t, rotate(theta[lowerFRLId], 1, 0, 0));
+    t = translate(0.0, upperFLH-0.1, 0.0);
+    t = mult(t, rotate(theta[lowerFRLId], 0, 0, 1));
     figure[lowerFRLId] = createNode(t, lowerFRL, null, null);
     break;
 
     case lowerRLLId:
-    t = translate(0.0, upperRLH, 0.0);
-    t = mult(t, rotate(theta[lowerRLLId], 1, 0, 0));
+    t = translate(0.0, upperRLH-0.1, 0.0);
+    t = mult(t, rotate(theta[lowerRLLId], 0, 0, 1));
     figure[lowerRLLId] = createNode(t, lowerRLL, null, null);
     break;
 
     case lowerRRLId:
-    t = translate(0.0, upperRLH, 0.0);
-    t = mult(t, rotate(theta[lowerRRLId], 1, 0, 0));
+    t = translate(0.0, upperRLH-0.1, 0.0);
+    t = mult(t, rotate(theta[lowerRRLId], 0, 0, 1));
     figure[lowerRRLId] = createNode(t, lowerRRL, null, null);
     break;
 
     case tailId:
-    t = translate(0.0, 0.0, 0.5);
-    t = mult(t, rotate(theta[tailId], 1, 0, 0));
+    t = translate(-(0.48 * bodyW), bodyH-0.1, 0.0);
+    t = mult(t, rotate(theta[tailId], 0, 0, 1));
     figure[tailId] = createNode(t, tail, null, null);
     break;
   }
@@ -287,7 +291,7 @@ function quad(a, b, c, d){
 
 function cube(){
   quad(1, 0, 3, 2);
-  quad(2, 3, 3, 6);
+  quad(2, 3, 7, 6);
   quad(3, 0, 4, 7);
   quad(6, 5, 1, 2);
   quad(4, 5, 6, 7);
@@ -325,11 +329,72 @@ window.onload = function init() {
     gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 
+
+    document.getElementById("slider0").onchange = function(event) {
+      theta[bodyId ] = event.target.value;
+      initNodes(bodyId);
+  };
+
+    document.getElementById("walkButton").onclick = function(){
+      clear();
+      walk();
+      }
+
     for(i = 0; i<=numNodes; i++) initNodes(i);
     render();
 }
 
+function clear(){
+    Sequence = 2;
+}
 
+function walk()
+{
+
+    Sequence = 0;
+	theta[bodyId] = 0;
+    initNodes(bodyId);
+
+    setInterval(function(){ walkLoop(); }, speed);
+
+function walkLoop()
+{
+
+  if (theta[upperFLLId] > 160 && Sequence == 0)
+  {
+      theta[upperFLLId] -= 1;
+      initNodes(upperFLLId);
+      theta[upperRLLId] -= 1;
+      initNodes(upperRLLId);
+      theta[upperFRLId] += 1;
+      initNodes(upperFRLId);
+      theta[upperRRLId] += 1;
+      initNodes(upperRRLId);
+      theta[tailId] -= 1;
+      initNodes(tailId);
+      if(theta[upperFLLId] == 160){
+        Sequence = 1;
+      }
+    }
+    if (theta[upperFLLId] <= 185 && Sequence == 1)
+	{
+      theta[upperFLLId] += 1;
+      initNodes(upperFLLId);
+      theta[upperRLLId] += 1;
+      initNodes(upperRLLId);
+      theta[upperFRLId] -= 1;
+      initNodes(upperFRLId);
+      theta[upperRRLId] -= 1;
+      initNodes(upperRRLId);
+      theta[tailId] += 1;
+      initNodes(tailId);
+      if(theta[upperFLLId] == 185)
+	  {
+        Sequence = 0;
+    }
+  }
+}
+}
 var render = function() {
 
         gl.clear( gl.COLOR_BUFFER_BIT );
