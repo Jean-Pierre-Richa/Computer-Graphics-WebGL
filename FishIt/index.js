9bde31c691d7763c5157657bc047b1aa6c1cc6de
Jean Pierre Richa <jeanpierrericha@Jeans-MacBook-Pro.local>
2018-10-28 19:33:50 +0100

    add fishes movement, and collision detection

--- FishIt/index.js
+++ FishIt/index.js
@@ -1,3 +1,21 @@
+var pesce = [];
+var boat;
+var boatFlag = false;
+var fishFlag = false;
+var flag = false;
+var helper;
+var boxes = [];
+var movingCubes = [];
+var collidableMeshList = [];
+var container, stats;
+var scene, renderer, camera;
+var light, light2;
+var progress;
+var quickTime = false;
+var keyboard = new KeyboardState();
+var bar = document.getElementById("bar-1");
+var xAxis = new THREE.Vector3(1, 0, 0);
+
 // Building the hierarchical structure
 var lighthouseId = 0;
 var boatId = 1;
@@ -63,6 +81,24 @@ function traverse(Id){
   if (figure[Id].sibling != null) traverse(figure[Id].sibling);
 }
 
+///////////////////
+//QUICKTIME EVENT
+///////////////////
+
+function SetBarsAsUndefined() {
+    bar.setUndeterminated(1);
+}
+
+function SetBarsAsDefined() {
+    bar.setUndeterminated(0);
+}
+
+function UpdateBars() {
+    bar.progress++;
+    if (document.getElementById("bar-1").progress < 100) {
+        setTimeout(UpdateBars, 10);
+    }
+}
 
 // Converts from degrees to radians.
 Math.radians = function (degrees) {
@@ -74,48 +110,92 @@ Math.degrees = function (radians) {
     return radians * 180 / Math.PI;
 };
 
-// OGGETTI
-var pesce = [];
-var boat;
-var boatFlag = false;
-var fishFlag = false;
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
-var scene = new THREE.Scene();
-scene.fog = new THREE.Fog(0xcce0ff, 500, 10000);
 
-///////////////////
-//CAMERA
-///////////////////
 
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 100000);
-// camera.position.set(-43, 872, 506);
-var controls = new THREE.OrbitControls(camera);
-controls.update();
+init();
+function init() {
+    ///////////////////
+    //CAMERA
+    ///////////////////
 
-///////////////////
-//LIGHTS & SHADOWS
-///////////////////
+    container = document.getElementById('container');
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
-document.body.appendChild(renderer.domElement);
+    scene = new THREE.Scene();
+    scene.fog = new THREE.Fog(0xcce0ff, 500, 10000);
 
-var light2 = new THREE.AmbientLight(0x404040); // soft white light
-scene.add(light2);
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 100000);
+    // camera.position.set(159, 10, 552);
 
-var light = new THREE.DirectionalLight(0xffffff, 3);
-// light.position.set(400, 400, 400);
-light.position.set(0, 200, -3000);
+    //ORBIT CONTROL
+    // var controls = new THREE.OrbitControls(camera);
+    // controls.update();
+    //
+    // camera.lookAt(0, 0, 0);
+    // window.addEventListener('mousemove', function (e) {
+    //     var mouse3D = new THREE.Vector3(
+    //         ((event.clientX / window.innerWidth) * 2 - 1) * 300,
+    //         (-(event.clientY / window.innerHeight) * 2 + 1) * 300,
+    //         0.5);
+    //     camera.lookAt(mouse3D);
+    // });
 
-light.castShadow = true;
-scene.add(light);
-//Set up shadow properties for the light
-light.shadow.mapSize.width = 512; // default
-light.shadow.mapSize.height = 512; // default
-light.shadow.camera.near = 0.5; // default
-light.shadow.camera.far = 500 // default
+    /* IF YOU NEED A 360 DEGREES CAMERA, YOU CAN USE POINTERLOCKCONTROL
+    EXAMPLE IS DINO:
+    https://codepen.io/MSEdgeDev/pen/NpKejy */
+
+    /* var looking = new THREE.PointerLockControls( camera );
+    scene.add( looking.getObject() ); */
+    //looking.enabled = true; // Turns on camera rotating with mouse
+
+
+    ///////////////////
+    //LIGHTS & SHADOWS
+    ///////////////////
+
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
+
+    light2 = new THREE.AmbientLight(0x404040); // soft white light
+    scene.add(light2);
+
+    light = new THREE.DirectionalLight(0xffffff, 3);
+    light.position.set(0, 200, -3000);
+    light.castShadow = true;
+    scene.add(light);
+    //Set up shadow properties for the light
+    light.shadow.mapSize.width = 512; // default
+    light.shadow.mapSize.height = 512; // default
+    light.shadow.camera.near = 0.5; // default
+    light.shadow.camera.far = 500 // default
+
+    container.innerHTML = "";
+    container.appendChild(renderer.domElement);
+
+    //STATS TOP LEFT
+    stats = new Stats();
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.top = '0px'
+    container.appendChild(stats.domElement);
+
+    //PROGRESS BAR
+    progress = document.getElementById('progressBar');
+    progress.style.position = 'absolute';
+    progress.style.top = '50%';
+    progress.style.left = '50%';
+    progress.style.visibility = 'hidden';
+    container.appendChild(progress);
+
+    window.addEventListener('resize', onWindowResize, false);
+}
 
 ///////////////////
 //WATER
@@ -343,24 +423,36 @@ function rodc(){
 
 //Adding the fishes
 loader.load(
-    // resource URL
     '/models/fish/scene.gltf',
-    // called when the resource is loaded
     function (gltf) {
-
         gltf.scene.scale.set(0.05, 0.05, 0.05);
         gltf.scene.castShadow = true;
-
         for (var i = 0; i < 100; i++) {
-
             x = THREE.Math.randFloatSpread(1000);
             z = THREE.Math.randFloatSpread(1000);
             gltf.scene.position.set(x, -20, z);
             gltf.scene.name = "pesce" + i.toString();
-            gltf.scene.rotation.y = Math.random();
+            gltf.scene.rotation.y = Math.random() * 6.28 - 3.14;
             pesce[i] = THREE.Object3D.prototype.clone.call(gltf.scene)
 
+            //COLLISION BOXES
+            var center = new THREE.Vector3();
+            var box = new THREE.Box3().setFromObject(pesce[i]);
+            var cubeGeometry = new THREE.BoxGeometry(box.getSize(center).x, box.getSize(center).y, box.getSize(
+                center).z);
+            var wireMaterial = new THREE.MeshBasicMaterial({
+                color: 0xff0000,
+                wireframe: true
+            });
+            movingCubes[i] = new THREE.Mesh(cubeGeometry, wireMaterial);
+            movingCubes[i].position.x = pesce[i].position.x;
+            movingCubes[i].position.y = pesce[i].position.y + 2.5;
+            movingCubes[i].position.z = pesce[i].position.z;
+
+            collidableMeshList.push(movingCubes[i]);
+
             scene.add(pesce[i]);
+            scene.add(movingCubes[i]);
         }
     },
     // called while loading is progressing
@@ -376,60 +468,122 @@ loader.load(
 );
 
 
-///////////////////
-//FISH MOVEMENT
-///////////////////
+  ///////////////////
+  //FISH MOVEMENT
+  ///////////////////
 
-var xAxis = new THREE.Vector3(1, 0, 0);
+  function movimentoPesce(oggetto) {
+      for (var i = 0; i < oggetto.length; i++) {
+          var randDegrees = Math.floor(Math.random() * 720) - 360;
+          var randRadians = Math.radians(randDegrees);
+          var x = 0.5 * Math.cos(randRadians);
+          var z = 0.5 * Math.sin(randRadians);
+          if (oggetto[i].position.x + x > 500 || oggetto[i].position.x + x < -500 || oggetto[i].position.z + z >
+              500 || oggetto[i].position.z + z < -500) {
+              var rand = THREE.Math.randFloatSpread(1000);
+              oggetto[i].position.set(0, -20, 0);
+          }
 
-function movimentoPesce(oggetto) {
+          function r1(oggetto, i, cubo) {
 
-    fishFlag = false;
-    for (var i = 0; i < oggetto.length; i++) {
-        var randDegrees = Math.floor(Math.random() * 360) - 180;
-        var randRadians = Math.radians(randDegrees);
-        var x = 2 * Math.cos(randRadians);
-        var z = 2 * Math.sin(randRadians);
-        if (oggetto[i].position.x + x > 500 || oggetto[i].position.x + x < -500 || oggetto[i].position.z + z >
-            500 || oggetto[i].position.z + z < -500) {
-            continue;
-        }
-        if (randRadians > oggetto[i].rotation.y) {
-            while (randRadians > oggetto[i].rotation.y) {
-                //setTimeout(function () {
-                    oggetto[i].rotation.y += 0.017;
-                    oggetto[i].translateOnAxis(xAxis, -0.5);
-                //}, 100);
-            }
-        } else {
-            while (randRadians < oggetto[i].rotation.y) {
-                //setTimeout(function () {
-                    oggetto[i].rotation.y -= 0.017;
-                    oggetto[i].translateOnAxis(xAxis, -0.5);
-                //}, 100);
-            }
-        }
-    }
-    fishFlag = true;
-}
+              //TO SLOW DOWN FISH MOVEMENT, UNCOMMENT SETTIMEOUT
+              setTimeout(function () {
+              if (randRadians > oggetto.rotation.y) {
+                  var deltax = oggetto.position.x;
+                  var deltaz = oggetto.position.z;
+                  oggetto.rotation.y += 0.17;
+                  oggetto.translateOnAxis(xAxis, -0.5);
+                  deltax = oggetto.position.x - deltax;
+                  deltaz = oggetto.position.z - deltaz;
+                  cubo.position.x += deltax;
+                  cubo.position.z += deltaz;
+                  r1(oggetto, i, cubo);
+              }
+            }, 300);
+          }
 
-if (fishFlag) {
-    setTimeout(function(){movimentoPesce(pesce);
-    }, 100);
+          function r2(oggetto, i, cubo) {
+              setTimeout(function () {
+              if (randRadians < oggetto.rotation.y) {
+                  var deltax = oggetto.position.x;
+                  var deltaz = oggetto.position.z;
+                  oggetto.rotation.y -= 0.17;
+                  oggetto.translateOnAxis(xAxis, -0.5);
+                  deltax = oggetto.position.x - deltax;
+                  deltaz = oggetto.position.z - deltaz;
+                  cubo.position.x += deltax;
+                  cubo.position.z += deltaz;
+                  r2(oggetto, i, cubo);
+              }
+            }, 300);
+          }
 
-}
+          randRadians = randRadians + oggetto[i].rotation.y
+          if (randRadians > oggetto[i].rotation.y) {
+              r1(oggetto[i], i, movingCubes[i]);
+          } else {
+              r2(oggetto[i], i, movingCubes[i]);
+          }
+      }
+  }
+
 function polarToCartesian(radius, angle) {
     return [radius * Math.cos(angle), radius * Math.sin(angle)]
 }
+
+function collision(obj) {
+    for (var i = 0; i < obj.length; i++) {
+        var originPoint = movingCubes[i].position.clone();
+        for (var vertexIndex = 0; vertexIndex < movingCubes[i].geometry.vertices.length; vertexIndex++) {
+            var localVertex = movingCubes[i].geometry.vertices[vertexIndex].clone();
+            var globalVertex = localVertex.applyMatrix4(movingCubes[i].matrix);
+            var directionVector = globalVertex.sub(movingCubes[i].position);
+
+            var ray = new THREE.Raycaster(originPoint, directionVector.clone().normalize());
+            var collisionResults = ray.intersectObjects(collidableMeshList);
+            if (collisionResults.length > 0 && collisionResults[0].distance < directionVector.length())
+                console.log(" Hit ");
+        }
+    }
+}
+
 var animate = function () {
     setTimeout( function() {
         requestAnimationFrame( animate );
 
-    }, 1000 / 40 );
+    }, 1000 / 30 );
+    var time = performance.now() * 0.001;
+
+
+    stats.update();
+    collision(pesce);
+    keyboard.update();
+
+    if (quickTime) {
+        flag = false;
+        bar.progress = 0;
+        progress.style.visibility = 'visible';
+        bar.style.visibility = 'visible';
+        UpdateBars();
+        quickTime = false;
+    }
+
     var time = performance.now() * 0.001;
     if (boatFlag) {
         boat.rotation.x = Math.sin(time) * 0.015;
     }
+
+    if (flag) {
+        movimentoPesce(pesce);
+    }
+
+    if (keyboard.down("space")) {
+        bar.progress = 100;
+        bar.style.visibility = 'hidden';
+        progress.style.visibility = 'hidden';
+        flag = true;
+    }
+
     renderer.render(scene, camera);
 };
 animate();
